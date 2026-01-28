2749d0db4e1c825f13d5d252da4595a4a3f92722
DianaBych <dianavika18@gmail.com>
2026-01-28 20:35:20 +0300

    feat: add check not integer

--- task1.ts
+++ task1.ts
@@ -1,1 +1,13 @@
 // Написать функцию, которая будет высчитывать сумму чисел от нуля, до параметра, который мы в неё передаем.
+function sumOfNumbers(lastNumber: number) {
+  let sum = 0;
+  let sumi = 0;
+  for (let startNumber = 0; startNumber <= lastNumber; startNumber++) {
+    sum += startNumber;
+  }
+  if (!Number.isInteger(lastNumber))  {
+    sumi = sum + lastNumber;
+  }
+  console.log(sumi);
+}
+sumOfNumbers(2.5);
